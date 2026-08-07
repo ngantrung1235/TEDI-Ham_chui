28812b658fac1f979b733f9dbb88e2c17430c93f
bigdickboy54 <tuthinh5405@gmail.com>
2026-08-07 18:14:51 +0700

    Xong tạo Addin

---
+++ TEDI-Ham_chui/TEDI-Ham_chui/ExternalCommands/Tao_tho.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using TEDI_Ham_chui.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEDI_Ham_chui.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Tao_tho:IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, Autodesk.Revit.DB.ElementSet elements)
+        {
+            try
+            {
+                
+                Window1 viewBoctach=new Window1();
+                viewBoctach.Show();
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ TEDI-Ham_chui/TEDI-Ham_chui/MainClass.cs
@@ -0,0 +1,50 @@
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.IO;
+namespace TEDI_Ham_chui
+{
+    
+        public class MainClass : IExternalApplication
+        {
+            public Result OnStartup(UIControlledApplication application)
+            {
+                //Add a new ribbon tab
+                application.CreateRibbonTab("Tedi-Ham_chui");
+
+                // Add a new ribbon panel
+                RibbonPanel ribbonPanel = application.CreateRibbonPanel("Tedi-Ham_chui", "NewRibbonPanel");
+
+
+                //tạo 1 nút button tạo phần thô
+                // Create a push button to trigger a command add it to the ribbon panel.
+                string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+                string assemblyDir = Path.GetDirectoryName(thisAssemblyPath);
+
+                PushButtonData buttonData = new PushButtonData("cmdTaotho",
+                   "Tạo thô", thisAssemblyPath, "TEDI_Ham_chui.ExternalCommands.Tao_tho");
+
+                PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+
+                // Optionally, other properties may be assigned to the button
+                // a) tool-tip
+                pushButton.ToolTip = "Say hello to the entire world.";
+
+                // b) large bitmap
+                string stampIconPath = Path.Combine(assemblyDir, "Assets", "icons", "stamp32.png");
+                Uri uriImage = new Uri(stampIconPath);
+                BitmapImage largeImage = new BitmapImage(uriImage);
+                pushButton.LargeImage = largeImage;
+
+
+                return Result.Succeeded;
+            }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // nothing to clean up in this simple case
+            return Result.Succeeded;
+        }
+    }
+
+}
